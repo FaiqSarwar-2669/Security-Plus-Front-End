b09c493482dc5d1702eacc650f43535971e2bad8
FaiqSarwa-2669 <code.faiq786@gmail.com>
2024-03-13 14:25:54 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'security_Plus_Front_End';
+}
