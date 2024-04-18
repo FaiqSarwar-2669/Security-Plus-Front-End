018f9d047f38516cfdec30ba6a2552262a16424c
FaiqSarwa-2669 <code.faiq786@gmail.com>
2024-04-18 07:26:35 +0500

    forget password api completed

--- src/app/forget-password/forget-password.component.ts
+++ src/app/forget-password/forget-password.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Service } from '../services';
+import Swal from 'sweetalert2';
+import { forgetPassword } from '../models/model';
 
 @Component({
   selector: 'app-forget-password',
@@ -7,4 +11,43 @@ import { Component } from '@angular/core';
 })
 export class ForgetPasswordComponent {
 
+  forgetPasswordData: forgetPassword = {
+    'email': ''
+  }
+
+  constructor(private services: Service, private routes: Router) {
+
+  }
+
+  forget() {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const email = this.forgetPasswordData.email || '';
+    if (this.forgetPasswordData.email?.trim() === '') {
+      Swal.fire('Enter the Email');
+      return;
+    } else if (!emailPattern.test(email)) {
+      Swal.fire('Invalid Email');
+      return;
+    } else {
+      this.services.forgetPassword(this.forgetPasswordData).then((res: any) => {
+        if (res && res.message) {
+          Swal.fire({
+            icon: 'success',
+            title: res.message
+          })
+          this.forgetPasswordData.email = '';
+          this.routes.navigate(['/login']);
+        }
+      }).catch((err: any) => {
+        console.log(err)
+        if(err && err.error){
+
+          Swal.fire({
+            icon: 'error',
+            title: err.error.error
+          })
+        }
+      })
+    }
+  }
 }
