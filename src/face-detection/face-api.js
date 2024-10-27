c3e3619d8323d52b60130a1353807b686b4636bb
chqasimfarooq <chqasimfarooq13@gmail.com>
2024-10-27 11:57:45 +0500

    correct error in face detection and set the index.css page

--- src/face-detection/face-api.js
+++ src/face-detection/face-api.js
@@ -1505,7 +1505,7 @@
   function isMediaLoaded(media) {
       var _a = env.getEnv(), Image = _a.Image, Video = _a.Video;
       return (media instanceof Image && media.complete)
-          || (media instanceof Video && media.readyState >= 3);
+          || (media instanceof Video && media.readyState >= 4);
   }
 
   function awaitMediaLoaded(media) {
